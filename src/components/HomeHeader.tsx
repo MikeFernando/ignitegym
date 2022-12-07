c3e500d2e4089e2b551145d9f54b995192eafd08
mikera <mike.silva@pixter.com.br>
2022-12-07 16:10:40 -0300

    finish component HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,11 @@
-import { VStack, Text, Heading, HStack } from 'native-base'
+import { VStack, Text, Heading, HStack, Icon } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+
 import { UserFoto } from './UserFoto'
 
 export const HomeHeader = () => {
   return (
-    <HStack pt={16} pb={5} px={8} alignItems='center'>
+    <HStack bg='gray.500' pt={16} pb={5} px={8} alignItems='center'>
       <UserFoto 
         source={{ uri: 'https://github.com/MikeFernando.png' }}
         alt='Imagem do usuário'
@@ -11,13 +13,20 @@ export const HomeHeader = () => {
         mr={4}
       />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color='gray.100' fontSize='md'>
           Olá</Text>
         <Heading color='gray.100' fontSize='md'>
           Mike Fernando
         </Heading>
       </VStack>
+
+      <Icon 
+        as={MaterialIcons}
+        name='logout'
+        color='gray.200'
+        size={7}
+      />
     </HStack>
   )
 }
