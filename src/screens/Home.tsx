b3f2f5733323079375decb2eac4f3f934429b545
mikera <mike.silva@pixter.com.br>
2022-12-07 18:09:39 -0300

    listing exercise

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
-import { FlatList, VStack } from 'native-base'
+import { FlatList, Heading, HStack, Text, VStack } from 'native-base'
 
 import { HomeHeader } from '@components/HomeHeader'
 import { Group } from '@components/Group'
+import { ExerciseCard } from '@components/ExerciseCard'
 
 export const Home = () => {
   const [ groups, setGroups ] = useState(['costas', 'Bícipes', 'Trícipes', 'Perna', 'Ombro'])
@@ -23,12 +24,28 @@ export const Home = () => {
             />
           )}
           horizontal
-          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
           _contentContainerStyle={{ px: 8 }}
           my={10}
           maxH={10}
           minH={10}
         />
+
+       <VStack flex={1} px={8}>
+          <HStack justifyContent='space-between' mb={5}>
+              <Heading color='gray.200' fontSize='md'>
+                  Exercícios
+              </Heading>
+
+              <Text color='gray.200' fontSize='sm'>
+                  5
+              </Text>
+          </HStack>
+
+          <ExerciseCard />
+          <ExerciseCard />
+
+       </VStack>
     </VStack>
   )
 }
